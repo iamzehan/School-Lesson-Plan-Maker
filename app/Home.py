1ef15ba3bf5af71836aa1752c4ce23e56215a91c
Zehan <zehan.kareem@gmail.com>
2024-01-27 22:05:35 +0600

    changed logo

--- app/Home.py
+++ app/Home.py
@@ -1,6 +1,6 @@
 import streamlit as st 
 
 st.set_page_config(page_title="Home", page_icon="🏠")
-st.markdown("""<center><img height=100 width=100 style='border-radius: 50px' src="https://shorturl.at/imMSW" </center>""", unsafe_allow_html=True)
+st.markdown("""<center><img height=100 width=100 style='border-radius: 50px' src="https://www.al-buroojbd.com/img/site/1650527711.png" </center>""", unsafe_allow_html=True)
 st.markdown("<center><h1>🏫</h1></center>", unsafe_allow_html=True)
 
